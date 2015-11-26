9cf8ad09d53a7430f08de04958322af5c5272c88
CrowleyLucky <shumanchang826@gmail.com>
2015-11-27 00:35:38 +0800

    update gitignore: ignore .class and .caches

--- PCAgent/src/com/crowley/util/Constants.java
+++ PCAgent/src/com/crowley/util/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
 	public static final String MAIN_FRAME_TITLE = "DesktopController";
 	public static final String INPUT_IP_HINT = "Input IP address on Android client:";
 	public static final String AUTHOR_INFO = "Made by Crowley";
+	public static final int SERVICE_PORT = 6666;
 	
 }
